647e55e79c07f42ff8710f5e4aeab3f575c19cbb
jsmon <jsmon.dev@gmail.com>
2021-02-02 16:41:34 +0000

    Added Header

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = (): React.ReactElement<{}, 'header'> => (
+    <header className="main-header">
+        <h1>James Simon</h1>
+    </header>
+);
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import MetaData from '../components/MetaData';
+import Header from '../components/Header';
+
 const Home = () => {
+    const isSSR = typeof window === 'undefined';
+
+    if (!isSSR) {
+        const theme = (localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark')) as 'light' | 'dark';
+        localStorage.setItem('theme', theme);
+        document.querySelector('html')!.dataset.theme = theme;
+    }
+
     return (
-        <div></div>
+        <div>
+            <MetaData />
+            <Header />
+        </div>
     );
 };
 

a1cbcecb8183c07f7114c4260e2618470e7fccd5
jsmon <jsmon.dev@gmail.com>
2021-02-02 17:09:16 +0000

    Added keywords and updated date revised

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -8,11 +8,11 @@ const MetaData = () => (
         <title>James Simon</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="keywords" content="html, css, javascript, js, typescript, ts, reactjs, react, nodejs, node, npm, frontend, backend, full-stack, expressjs, express, nextjs, next, gatsbyjs, gatsby, kotlin, kt, android, applications, application, apps, app, create-react-app, cra, api, developer, developing, develop, programmer, programming, program, code, coding, coder, hack, hacker, hacking, ethical-hacking, facebook, amazon, apple, netfilx, google, fang, faang, function, functions, variable, variables, web-app, website, web, internet, world-wide-web" />
+        <meta name="keywords" content="james, simon, james-simon, james simon, html, css, javascript, js, typescript, ts, react.js, reactjs, react, node.js, nodejs, node, npm, frontend, backend, full-stack, full stack, express.js, expressjs, express, next.js, nextjs, next, gatsby.js, gatsbyjs, gatsby, kotlin, kt, android, applications, application, apps, app, create react app, create-react-app, cra, api, web-development, web development, develpoment, web-developer, web developer, developer, web-designer, web designer, designer, web-devloping, web developing, developing, web-designing, web designing, designing, web-develop, web develop, develop, web-design, web design, design, programmer, programming, program, code, coding, coder, hack, hacker, hacking, ethical hacking, ethical-hacking, facebook, amazon, apple, netfilx, google, fang, faang, function, functions, variable, variables, web application, web-application, web app, web-app, website, web, internet, net, world wide web, world-wide-web, web, algorithms, linux, ubuntu, kali, kali-linux, kali linux, debian, windows, windows-10, windows 10, mac, macos, osx, command, commands, git, github, open-source, open source, oss, software, hardware, seo, search-engine-optimisation, search engine optimisation, firebase, firestore, firebase-firestore, firebase firestore, python, py" />
         <meta name="description" content="James Simon is a full stack web developer who uses React.js, Next.js, Node.js, TypeScript, JavaScript, and more." />
         <meta name="subject" content="Portfolio" />
         <meta name="language" content="en" />
-        <meta name="revised" content="Tuesday, February 2nd, 2021, 3:28 pm" />
+        <meta name="revised" content="Tuesday, February 2nd, 2021, 5:09 pm" />
         <meta name="author" content="James Simon, jsmon.dev@gmail.com" />
         <meta name="designer" content="James Simon, jsmon.dev@gmail.com" />
         <meta name="reply-to" content="jsmon.dev@gmail.com" />

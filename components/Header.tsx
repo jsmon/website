5f915e2318a02834420bb2f0d0d447d22bc17ea0
jsmon <jsmon.dev@gmail.com>
2021-02-02 16:52:43 +0000

    Updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = (): React.ReactElement<{}, 'header'> => (
     <header className="main-header">
-        <h1>James Simon</h1>
+        <h1>Hello, I'm James Simon</h1>
     </header>
 );
 

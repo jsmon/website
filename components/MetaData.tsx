473e2e6ef14bde2c62916cfce3971315429879c1
jsmon <jsmon.dev@gmail.com>
2021-02-02 16:00:12 +0000

    Added a lot of meta tags

---
+++ components/MetaData.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+const MetaData = () => (
+    <Head>
+        { /* Yes, I tried to use every meta tag. SEO, right? */ }
+        <title>James Simon</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="html, css, javascript, js, typescript, ts, reactjs, react, nodejs, node, npm, frontend, backend, full-stack, expressjs, express, nextjs, next, gatsbyjs, gatsby, kotlin, kt, android, applications, application, apps, app, create-react-app, cra, api, developer, developing, develop, programmer, programming, program, code, coding, coder, hack, hacker, hacking, ethical-hacking, facebook, amazon, apple, netfilx, google, fang, faang, function, functions, variable, variables, web-app, website, web, internet, world-wide-web" />
+        <meta name="description" content="James Simon is a full stack web developer who uses React.js, Next.js, Node.js, TypeScript, JavaScript, and more." />
+        <meta name="subject" content="Portfolio" />
+        <meta name="language" content="en" />
+        <meta name="revised" content="Tuesday, February 2nd, 2021, 3:28 pm" />
+        <meta name="author" content="James Simon, jsmon.dev@gmail.com" />
+        <meta name="designer" content="James Simon, jsmon.dev@gmail.com" />
+        <meta name="reply-to" content="jsmon.dev@gmail.com" />
+        <meta name="owner" content="James Simon, jsmon.dev@gmail.com" />
+        <meta name="url" content="https://jsmon.github.io" />
+        <meta name="identifier-URL" content="https://jsmon.github.io" />
+
+        <meta property="og:title" content="James Simon" />
+        <meta property="og:type" content="portfolio website" />
+        <meta property="og:url" content="https://jsmon.github.io" />
+        <meta property="og:site_name" content="GitHub Pages" />
+        <meta property="og:description" content="James Simon is a full stack web developer who uses React.js, Next.js, Node.js, TypeScript, JavaScript, and more." />
+        <meta property="og:email" content="jsmon.dev@gmail.com" />
+        <meta property="og:locale" content="en_GB" />
+        <meta property="og:country-name" content="United Kingdom" />
+    </Head>
+);
+
+export default MetaData;

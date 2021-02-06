bc6f4e9e513d711d81239109911adf97a31b4778
James Simon <jsmon.dev@gmail.com>
2021-02-06 14:20:14 +0000

    Removed dotenv because Next loads .env variables

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,5 @@
 import admin from 'firebase-admin';
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({ path: '.env.development.local' });
-}
-
 if (admin.apps.length === 0) admin.initializeApp({
     credential: admin.credential.cert(JSON.parse(process.env.SERVICE_ACCOUNT_KEY!)),
     storageBucket: 'james-simon-s-website.appspot.com'

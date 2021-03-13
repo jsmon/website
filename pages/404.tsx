46a90f0fe7008dce49548bda6c3238cadfa4c7ec
James Simon <jsmon.dev@gmail.com>
2021-03-13 21:09:58 +0000

    Added jsmon-preview.vercel.app as a valid domain for this website

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,7 +7,7 @@ const NotFoundPage = (): React.ReactElement<{}, 'main'> => {
 
     useEffect(() => {
         setTheme((localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark')) as 'light' | 'dark');
-        if (process.env.NODE_ENV === 'production' && window.location.hostname !== 'jsmon.vercel.app') {
+        if (process.env.NODE_ENV === 'production' && !['jsmon.vercel.app', 'jsmon-preview.vercel.app'].includes(window.location.hostname)) {
             window.location.replace(`https://jsmon.vercel.app${window.location.pathname}`);
         }
     }, []);

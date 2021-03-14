8d5bd1d7e6ba2f63ab0c472cb81ec994d73b6a3e
James Simon <jsmon.dev@gmail.com>
2021-03-14 18:07:35 +0000

    Added LinkedIn and Twitter links

--- components/Links.tsx
+++ components/Links.tsx
@@ -6,12 +6,16 @@ const Links = (): React.ReactElement<{
 }, 'section'> => (
     <section id="where-to-find-me" className="p-2">
         <h2 className="font-bold text-2xl">Where to Find Me</h2>
-        <p>My website: <a className="text-blue-600 hover:underline" href="https://jsmon.vercel.app/" target="_blank" rel="noopener noreferrer">jsmon.vercel.app</a></p>
-        <p>GitHub: <a className="text-blue-600 hover:underline" href="https://github.com/jsmon" target="_blank" rel="noopener noreferrer">jsmon</a></p>
-        <p>StackOverflow: <a className="text-blue-600 hover:underline" href="https://stackoverflow.com/users/15100869" target="_blank" rel="noopener noreferrer">James Simon</a></p>
-        <p>npm: <a className="text-blue-600 hover:underline" href="https://www.npmjs.com/~jsmon1" target="_blank" rel="noopener noreferrer">jsmon1</a></p>
-        <p>Pluralsight: <a className="text-blue-600 hover:underline" href="https://app.pluralsight.com/profile/james-simon-20" target="_blank" rel="noopener noreferrer">James Simon</a></p>
-        <p>Email me: <a className="text-blue-600 hover:underline" href="mailto:jsmon.dev@gmail.com" target="_blank" rel="noopener noreferrer">jsmon.dev@gmail.com</a></p>
+        <ul>
+            <li>My website: <a className="text-blue-600 hover:underline focus:underline" href="https://jsmon.vercel.app/" target="_blank" rel="noopener noreferrer">jsmon.vercel.app</a></li>
+            <li>GitHub: <a className="text-blue-600 hover:underline focus:underline" href="https://github.com/jsmon" target="_blank" rel="noopener noreferrer">@jsmon</a></li>
+            <li>LinkedIn: <a className="text-blue-600 hover:underline focus:underline" href="https://www.linkedin.com/in/james-simon-a19baa206" target="_blank" rel="noopener noreferrer">James Simon</a></li>
+            <li>Twitter: <a className="text-blue-600 hover:underline focus:underline" href="https://twitter.com/jsmondev" target="_blank" rel="noopener noreferrer">@jsmondev</a></li>
+            <li>StackOverflow: <a className="text-blue-600 hover:underline focus:underline" href="https://stackoverflow.com/users/15100869" target="_blank" rel="noopener noreferrer">James Simon</a></li>
+            <li>npm: <a className="text-blue-600 hover:underline focus:underline" href="https://www.npmjs.com/~jsmon1" target="_blank" rel="noopener noreferrer">jsmon1</a></li>
+            <li>Pluralsight: <a className="text-blue-600 hover:underline focus:underline" href="https://app.pluralsight.com/profile/james-simon-20" target="_blank" rel="noopener noreferrer">James Simon</a></li>
+            <li>Email me: <a className="text-blue-600 hover:underline focus:underline" href="mailto:jsmon.dev@gmail.com" target="_blank" rel="noopener noreferrer">jsmon.dev@gmail.com</a></li>
+        </ul>
     </section>
 );
 

16fafba2b871cebcd9da68865c207fcd87f446a5
James Simon <jsmon.dev@gmail.com>
2021-03-22 21:21:58 +0000

    Updated meta descriptions

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -9,7 +9,7 @@ const MetaData = () => (
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content="james, simon, james-simon, james simon, jsmon, jsmon1, html, css, tailwind, tailwindcss, javascript, js, typescript, ts, react.js, reactjs, react, node.js, nodejs, node, npm, frontend, backend, full-stack, full stack, express.js, expressjs, express, next.js, nextjs, next, gatsby.js, gatsbyjs, gatsby, kotlin, kt, android, applications, application, apps, app, create react app, create-react-app, cra, api, data, database, databases, nosql, sql, mongodb, redisdb, couchdb, postgres, postgresql, mysql, mssql, mean, mern, mevn, async, asyncronous, web-development, web development, develpoment, web-developer, web developer, developer, web-designer, web designer, designer, web-devloping, web developing, developing, web-designing, web designing, designing, web-develop, web develop, develop, web-design, web design, design, programmer, programming, program, code, coding, coder, hack, hacker, hacking, ethical hacking, ethical-hacking, facebook, amazon, apple, netfilx, google, fang, faang, function, functions, variable, variables, bedrockcoder, BedrockCoder, bedrock-coder, bedrock coder, Bedrock-Coder, Bedrock Coder, bedrockcoder.net, BedrockCoder.net, web application, web-application, web app, web-app, website, web, internet, net, world wide web, world-wide-web, web, algorithms, linux, ubuntu, kali, kali-linux, kali linux, debian, windows, windows-10, windows 10, mac, macos, osx, command, commands, git, github, open-source, open source, oss, software, hardware, seo, search-engine-optimisation, search engine optimisation, firebase, firestore, firebase-firestore, firebase firestore, python, py binary-trees, binary trees, linked-list, linked-lists, stackoverflow, stack-overflow, stack overflow, pluralsight" />
-        <meta name="description" content="James Simon is a full stack web developer who uses React.js, Next.js, Node.js, TypeScript, JavaScript, and more." />
+        <meta name="description" content="James Simon is a full-stack developer who makes websites, APIs and other tools with TypeScript / JavaScript, Node.js, React.js, Next.js, and more." />
         <meta name="subject" content="Portfolio" />
         <meta name="language" content="en" />
         <meta name="author" content="James Simon, jsmon.dev@gmail.com" />
@@ -23,7 +23,7 @@ const MetaData = () => (
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://jsmon.dev/" />
         <meta property="og:site_name" content="James Simon" />
-        <meta property="og:description" content="James Simon is a full stack web developer who uses React.js, Next.js, Node.js, TypeScript, JavaScript, and more." />
+        <meta property="og:description" content="James Simon is a full-stack developer who makes websites, APIs and other tools with TypeScript / JavaScript, Node.js, React.js, Next.js, and more." />
         <meta property="og:email" content="jsmon.dev@gmail.com" />
         <meta property="og:locale" content="en_GB" />
         <meta property="og:country-name" content="United Kingdom" />

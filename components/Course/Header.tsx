949767cd0a963686f54bbc621a123c762f970fa7
James Simon <jsmon.dev@gmail.com>
2021-05-20 19:36:16 +0100

    Make image prop  types stricter and set image size

--- components/Course/Header.tsx
+++ components/Course/Header.tsx
@@ -4,7 +4,7 @@ interface Props {
     children: string;
     author: {
         name: string;
-        image: string;
+        image: `http${'s' | ''}://${string}`;
         link: `http${'s' | ''}://${string}`;
     };
     date: Date;
@@ -17,7 +17,7 @@ const Header = ({ children, author, date }: Props): React.ReactElement<{
         <p className="m-2">{ date.toLocaleDateString('en-GB') }</p>
         <h1 className="text-5xl text-center font-bold p-3.5">{ children }</h1>
         <p className="m-2 font-bold text-2xl p-2"><a href={author.link}>
-            <img src={author.image} alt={author.name} className="inline-block rounded-full" />
+            <img src={author.image} alt={author.name} className="inline-block rounded-full w-14" />
             <span className="p-2">{ author.name }</span>
         </a></p>
     </header>

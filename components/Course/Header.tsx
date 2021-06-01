661d8efd5fcdf89e635af17a9ea058b615b92180
James Simon <jsmon.dev@gmail.com>
2021-06-01 16:06:25 +0100

    Add ability to set a date when the article was last updated

--- components/Course/Header.tsx
+++ components/Course/Header.tsx
@@ -8,13 +8,15 @@ interface Props {
         link: `http${'s' | ''}://${string}.${string}`;
     };
     date: Date;
+    dateUpdated?: Date;
 }
 
-const Header = ({ children, author, date }: Props): React.ReactElement<{
+const Header = ({ children, author, date, dateUpdated }: Props): React.ReactElement<{
     children: React.ReactNode;
 }, 'header'> => (
     <header>
-        <p className="m-2">{ date.toLocaleDateString('en-GB') }</p>
+        <p className="m-2">{ date.toLocaleDateString('en-GB') }
+            { (dateUpdated?.getTime() ?? -Infinity) > date.getTime() && <span> - Last updated on { dateUpdated?.toLocaleDateString('en-GB') }</span> }</p>
         <h1 className="text-5xl text-center font-bold p-3.5">{ children }</h1>
         <p className="m-2 font-bold text-2xl p-2"><a href={author.link}>
             <img src={author.image} alt={author.name} className="inline-block rounded-full w-14" />
